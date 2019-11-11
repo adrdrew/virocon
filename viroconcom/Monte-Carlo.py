2edcb7018be0e691044f1eef5e72045dec4d7509
Adrian <adi97@t-online.de>
2019-11-11 23:00:40 +0100

    Beispiel

--- viroconcom/Monte-Carlo.py
+++ viroconcom/Monte-Carlo.py
@@ -1,77 +1,65 @@
 
-#data: WSPD APD
-#      xyz  xyz
-#      ...  ...
-#WSPD: WindSpeed
-#WVHT: significantWaveHight
-#APD: wavePeriode
-#
-# Hier erstmal nach dem Paper von elsevier Ocean Engineering
-#
-#Variablen:
-#Tz = wavePeriod
-#Hs = significantWaveHeight
-
-
-#
-#Daten aufbereiten.
-#
-
 import csv
+import matplotlib.pyplot as plt
+from viroconcom.fitting import Fit
+from viroconcom.contours import IFormContour
+import  scipy.stats as sts
+import numpy as np
+
+
 dataList = []
 
 with open('SampleDataBuoy44009Year2000', 'r') as data:
     reader = csv.reader(data, delimiter=',')
     for row in reader:
         dataList.append(row)
+
 dataList.pop(0)
-for i in dataList:
-    i.pop(1)
-dataList.pop(0)
-array = []
 array1 = []
 array2 = []
+array0 = []
 for i in dataList:
-    array1.append(i[0])
-    array2.append(i[1])
-array.append(array1)
-array.append(array2)
-#print(array1)
-for i in dataList:
-    i[0] = float(i[0])
-    i[1] = float(i[1])
-print(dataList)
-#Array1 sortieren
-array1 = sorted(array1)
-#print(array1)
-#
-#Durchschnitt
+    array0.append(float(i[0]))
+    array1.append(float(i[1]))
+    array2.append(float(i[2]))
 
-a = 0.0
-for i in array1:
-    a = a+float(i)
-mean = a/len(array1)
-print(mean)
+print(array0)
+fig1 = plt.figure()
+plt.plot(array1, array2, '.k')
+plt.show()
 
-#Varianz
-b = 0.0
-for i in array1:
-    b = b + ((float(i)-mean)*(float(i)-mean))
-var = b/(len(array1)-1)
-print(var)
+# The dependency for
+# the parameters must be given in the order shape, location, scale.
+dist_description_1 = {'name': 'Weibull_3p',
+                      'dependency': (None, None, None),
+                      'width_of_intervals': 1}
+dist_description_2 = {'name': 'Lognormal',
+                      'dependency': (0, None, 0),
+                      'functions': ('exp3', None, 'power3')}
+dist_structure = (dist_description_1, dist_description_2)
 
-#Standartabweichung
-import math
-s = math.sqrt(var)
-print(s)
+sea_state_sample = (array1, array2)
 
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy import stats
+my_fit = Fit(sea_state_sample, dist_structure)
+print(my_fit.mul_var_dist.distributions[0].name)
+print('Shape: ' + str(my_fit.mul_var_dist.distributions[0].shape))
+print('Location: ' + str(my_fit.mul_var_dist.distributions[0].loc))
+print('Scale: ' + str(my_fit.mul_var_dist.distributions[0].scale))
+print(my_fit.mul_var_dist.distributions[1])
+print(my_fit.mul_var_dist.latex_repr(['Hs', 'T']))
 
+fig2=plt.figure()
+plt.hist(array1, density=1)
+shape = my_fit.mul_var_dist.distributions[0].shape(0)
+loc = my_fit.mul_var_dist.distributions[0].loc(0)
+scale = my_fit.mul_var_dist.distributions[0].scale(0)
+x = np.linspace(0, 20, 100)
+f = sts.weibull_min.pdf(x, c=shape, loc=loc, scale=scale)
+plt.plot(x, f)
+plt.show()
 
-df = pd.DataFrame(dataList, columns=["x", "y"])
-sns.jointplot(x='x', y='y', data=df)
+c = IFormContour(my_fit.mul_var_dist, 50, 1, 50)
+fig3 = plt.figure()
+plt.plot(c.coordinates[0][0], c.coordinates[0][1])
+plt.plot(sea_state_sample[0], sea_state_sample[1], '.k')
 plt.show()
