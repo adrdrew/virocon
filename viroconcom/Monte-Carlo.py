3a5fdfe7aa99e47dc159d87cf183b7fccd6466b6
Adrian <adi97@t-online.de>
2019-11-22 16:11:50 +0100

    Coastdat test

--- viroconcom/Monte-Carlo.py
+++ viroconcom/Monte-Carlo.py
@@ -1,65 +1,87 @@
-
 import csv
+
 import matplotlib.pyplot as plt
-from viroconcom.fitting import Fit
-from viroconcom.contours import IFormContour
-import  scipy.stats as sts
+import netCDF4
 import numpy as np
+import scipy.stats as sts
+
+from viroconcom.contours import IFormContour
+from viroconcom.fitting import Fit
 
 
-dataList = []
+def get_data_coast():
+    coast = netCDF4.MFDataset('cD-2_WAM-North_Sea_hs_1965.nc', 'r')
+    coast2 = netCDF4.MFDataset('cD-2_WAM-North_Sea_tm2_1965.nc', 'r')
+    print(coast.variables['hs'][:])
+    return coast, coast2
 
-with open('SampleDataBuoy44009Year2000', 'r') as data:
-    reader = csv.reader(data, delimiter=',')
-    for row in reader:
-        dataList.append(row)
 
-dataList.pop(0)
-array1 = []
-array2 = []
-array0 = []
-for i in dataList:
-    array0.append(float(i[0]))
-    array1.append(float(i[1]))
-    array2.append(float(i[2]))
+def get_data_ndbc():
+    datalist = []
+    with open('SampleDataBuoy44009Year2000', 'r') as data:
+        reader = csv.reader(data, delimiter=',')
+        for row in reader:
+            datalist.append(row)
 
-print(array0)
-fig1 = plt.figure()
-plt.plot(array1, array2, '.k')
-plt.show()
+    datalist.pop(0)
+    array1 = []
+    array2 = []
+    array0 = []
+    for i in datalist:
+        array0.append(float(i[0]))
+        array1.append(float(i[1]))
+        array2.append(float(i[2]))
+    return array1, array2
 
-# The dependency for
-# the parameters must be given in the order shape, location, scale.
-dist_description_1 = {'name': 'Weibull_3p',
-                      'dependency': (None, None, None),
-                      'width_of_intervals': 1}
-dist_description_2 = {'name': 'Lognormal',
-                      'dependency': (0, None, 0),
-                      'functions': ('exp3', None, 'power3')}
-dist_structure = (dist_description_1, dist_description_2)
 
-sea_state_sample = (array1, array2)
+def show_data(coast, coast2):
+    print(coast)
+    fig1 = plt.figure()
+    plt.plot(coast, coast2, '.k')
+    plt.show()
 
-my_fit = Fit(sea_state_sample, dist_structure)
-print(my_fit.mul_var_dist.distributions[0].name)
-print('Shape: ' + str(my_fit.mul_var_dist.distributions[0].shape))
-print('Location: ' + str(my_fit.mul_var_dist.distributions[0].loc))
-print('Scale: ' + str(my_fit.mul_var_dist.distributions[0].scale))
-print(my_fit.mul_var_dist.distributions[1])
-print(my_fit.mul_var_dist.latex_repr(['Hs', 'T']))
 
-fig2=plt.figure()
-plt.hist(array1, density=1)
-shape = my_fit.mul_var_dist.distributions[0].shape(0)
-loc = my_fit.mul_var_dist.distributions[0].loc(0)
-scale = my_fit.mul_var_dist.distributions[0].scale(0)
-x = np.linspace(0, 20, 100)
-f = sts.weibull_min.pdf(x, c=shape, loc=loc, scale=scale)
-plt.plot(x, f)
-plt.show()
+# The dependency for the parameters must be given in the order shape, location, scale.
+def fit(array1, array2):
+    dist_description_1 = {'name': 'Weibull_3p',
+                          'dependency': (None, None, None),
+                          'width_of_intervals': 1}
+    dist_description_2 = {'name': 'Lognormal',
+                          'dependency': (0, None, 0),
+                          'functions': ('exp3', None, 'power3')}
+    dist_structure = (dist_description_1, dist_description_2)
 
-c = IFormContour(my_fit.mul_var_dist, 50, 1, 50)
-fig3 = plt.figure()
-plt.plot(c.coordinates[0][0], c.coordinates[0][1])
-plt.plot(sea_state_sample[0], sea_state_sample[1], '.k')
-plt.show()
+    sea_state_sample = (array1, array2)
+
+    my_fit = Fit(sea_state_sample, dist_structure)
+    print(my_fit.mul_var_dist.distributions[0].name)
+    print('Shape: ' + str(my_fit.mul_var_dist.distributions[0].shape))
+    print('Location: ' + str(my_fit.mul_var_dist.distributions[0].loc))
+    print('Scale: ' + str(my_fit.mul_var_dist.distributions[0].scale))
+    print(my_fit.mul_var_dist.distributions[1])
+    print(my_fit.mul_var_dist.latex_repr(['Hs', 'T']))
+
+    fig2 = plt.figure()
+    plt.hist(array1, density=1)
+    shape = my_fit.mul_var_dist.distributions[0].shape(0)
+    loc = my_fit.mul_var_dist.distributions[0].loc(0)
+    scale = my_fit.mul_var_dist.distributions[0].scale(0)
+    x = np.linspace(0, 20, 100)
+    f = sts.weibull_min.pdf(x, c=shape, loc=loc, scale=scale)
+    plt.plot(x, f)
+    plt.show()
+    return my_fit, sea_state_sample
+
+
+def show_contour(my_fit, sea_state_sample):
+    c = IFormContour(my_fit.mul_var_dist, 50, 1, 50)
+    fig3 = plt.figure()
+    plt.plot(c.coordinates[0][0], c.coordinates[0][1])
+    plt.plot(sea_state_sample[0], sea_state_sample[1], '.k')
+    plt.show()
+
+
+data0, data1 = get_data_ndbc()
+show_data(data0, data1)
+my_fit, sea_state_sample = fit(data0, data1)
+show_contour(my_fit, sea_state_sample)
