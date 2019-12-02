5eee353612145713eeb526040d4d3e859f0fcd35
lbekov <lbekov@uni-bremen.de>
2019-12-02 13:00:44 +0100

    update

--- viroconcom/imports.py
+++ viroconcom/imports.py
@@ -3,21 +3,30 @@
 # !/usr/bin/env python
 from ecmwfapi import ECMWFDataServer
 
-server = ECMWFDataServer(url="https://api.ecmwf.int/v1",key='5779c25ba27168b4e35275198c308319',email='lbekov@uni-bremen.de')
+def _get_data():
+    server = ECMWFDataServer(url="https://api.ecmwf.int/v1",key='5779c25ba27168b4e35275198c308319',email='lbekov@uni-bremen.de')
 
 
 
-server.retrieve({
-    "class": "ei",
-    "dataset": "interim",
-    "date": "2001-06-01/to/2001-06-30",
-    "expver": "1",
-    "grid": "0.75/0.75",
-    "levtype": "sfc",
-    "param": "229.140/232.140",
-    "step": "0",
-    "stream": "wave",
-    "time": "00:00:00/06:00:00/12:00:00/18:00:00",
-    "type": "an",
-    "target": "output",
-})
+    server.retrieve({
+        "class": "ei",
+        "dataset": "interim",
+        "date": "2001-06-01/to/2001-06-30",
+        "expver": "1",
+        "grid": "0.75/0.75",
+        "levtype": "sfc",
+        "param": "229.140/232.140",
+        "step": "0",
+        "stream": "wave",
+        "time": "00:00:00/06:00:00/12:00:00/18:00:00",
+        "type": "an",
+        "target": "output",
+    })
+
+import cfgrib
+
+def _test_get():
+    test = cfgrib.Dataset("Output")
+    print(test)
+
+_test_get()
